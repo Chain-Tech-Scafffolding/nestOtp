f3da9cf5058d4d676574c3839b3d4af6373a7998
Dhanraj Choudhary <choudharydhanraj239@gmail.com>
2024-03-10 13:04:27 +0530

    modules add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, RabbitmqModule, TodoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
